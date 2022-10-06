02f698372c50723a4a4d3a082bbb28219b4557bf
Rasmus Moorats <xx@nns.ee>
2022-10-06 16:15:42 +0300

    initial commit

---
+++ index.mjs
@@ -0,0 +1,4 @@
+import Hashes from "jshashes";
+import _ from "lodash-es";
+
+export { Hashes, _ };
---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import globals from 'rollup-plugin-node-globals';
+import builtins from 'rollup-plugin-node-builtins';
+import json from '@rollup/plugin-json';
+
+export default {
+  input: ['index.mjs'],
+  output: {
+    file: 'bundle.mjs',
+    format: 'esm',
+    exports: 'named',
+    inlineDynamicImports: true
+  },
+  plugins: [
+    nodeResolve({ preferBuiltins: false }),
+    commonjs(),
+    globals(),
+    builtins(),
+    json()
+  ]
+};
