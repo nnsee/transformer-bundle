963849041263ad817dcf552e3762a485193e9b24
Rasmus Moorats <xx@nns.ee>
2022-10-07 10:40:12 +0300

    use terser to minify bundle

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import globals from 'rollup-plugin-node-globals';
 import builtins from 'rollup-plugin-node-builtins';
 import json from '@rollup/plugin-json';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: ['index.mjs'],
@@ -17,6 +18,7 @@ export default {
     commonjs(),
     globals(),
     builtins(),
-    json()
+    json(),
+    terser()
   ]
 };
